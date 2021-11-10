e0a2d76427284ef7cef4cd85cc66b92bce6ee467
rikutoman12 <rikutoo1312@gmail.com>
2021-11-10 01:59:29 +0000

    投稿反映完了

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,17 @@
 class MenusController < ApplicationController
   def new
-    @menu =Menu.new
+    @menu = Menu.new
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    @menu.user_id = current_user.id
+    @menu.save
+    redirect_to menus_path
   end
 
   def index
-    
+    @menus = Menu.all
   end
 
   def show
@@ -17,5 +21,10 @@ class MenusController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def menu_params
+     params.require(:menu).permit(:title,:body,:user_id,:image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6bd2770039a453c11fee53b081669eeab1b68e2ed1bf3d158fb40bf968df602bb20730d5d3e423ad647ec98bed9fd0ce7acba1847ac6caf49b85160a0568277f'
