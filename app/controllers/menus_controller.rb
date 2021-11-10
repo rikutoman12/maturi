5907eab899977fa917e3f8270812680f890abeb7
rikutoman12 <rikutoo1312@gmail.com>
2021-11-10 01:17:14 +0000

    投稿機能完了

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,21 @@
+class MenusController < ApplicationController
+  def new
+    @menu =Menu.new
+  end
+
+  def create
+  end
+
+  def index
+    
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,4 @@
 class Menu < ApplicationRecord
+   belongs_to :user
+   attachment :image
 end
