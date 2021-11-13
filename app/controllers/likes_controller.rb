7883e1ffd2ea953a1777bc2e6cbd925b75f7f4f2
rikutoman12 <rikutoo1312@gmail.com>
2021-11-13 06:03:32 +0000

    いいね非同期中

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,6 @@ def create
   @like = current_user.likes.new(menu_id:@menu.id)
   @like.menu_id = @menu.id
   @like.save
-  redirect_to menu_path(@menu)
 end
 
 def destroy
@@ -13,7 +12,6 @@ def destroy
   @like = current_user.likes.find_by(menu_id:@menu.id)
   @like.menu_id = @menu.id
   @like.destroy
-  redirect_to menu_path(@menu)
 end
 
 end
