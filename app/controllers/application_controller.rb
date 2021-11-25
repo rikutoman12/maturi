f2ca9e7a73d0f8613a64c3270ff61a0cb69acb80
GitHubのユーザ名 <rikutoo1312@gmail.com>
2021-11-25 03:04:55 +0000

    投稿ページ編集ページ変更完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-   #indexページとaページ以外は会員以外ログイン出来ない
-  before_action :authenticate_user!,except: [:index,:a]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
