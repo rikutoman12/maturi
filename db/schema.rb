c2c38d77af794a3804961ad22ef097dc0615fe05
GitHubのユーザ名 <rikutoo1312@gmail.com>
2021-12-05 01:56:19 +0000

    カラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2021_12_04_092630) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
-    t.string "tags"
     t.integer "menu_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
